1efa19783059eb99dd9da05e096cca28eb7865cd
natneam <se.natneam.mesele@gmail.com>
2021-02-15 07:36:00 +0300

    editing the clock logic

--- analog clock/assets/js/app.js
+++ analog clock/assets/js/app.js
@@ -6,13 +6,20 @@ let secondHand = document.getElementById('second-hand');
 document.addEventListener('DOMContentLoaded', _=>{
     secondHand.style.transform = `rotate(${(new Date).getSeconds()}deg)`;
     minuteHand.style.transform = `rotate(${(new Date).getMinutes()}deg)`;
-    hourHand.style.transform = `rotate(${(new Date).getHours()%12}deg)`;
+    hourHand.style.transform = `rotate(${(new Date).getHours()*30}deg)`;
 });
 
 setInterval(rotateHands, 1000);
 
 function rotateHands(){
-    secondHand.style.transform = `rotateZ(${(new Date).getSeconds()*6}deg)`
-    minuteHand.style.transform = `rotateZ(${(new Date).getMinutes()*6}deg)`
-    hourHand.style.transform = `rotateZ(${((new Date).getHours()*30)}deg)`
+    let sec = (new Date).getSeconds();
+    let minute = (new Date).getMinutes();
+    let hour = (new Date).getHours();
+    secondHand.style.transform = `rotateZ(${sec*6}deg)`
+    minuteHand.style.transform = `rotateZ(${minute*6}deg)`
+    hourHand.style.transform = `rotateZ(${(hour*30 + minute*0.5)}deg)`
+
+    console.log(`Seconds: ${sec}`)
+    console.log(`Minutes: ${minute}`)
+    console.log(`Hours: ${hour}`)
 }
