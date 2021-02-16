aa9c2e770e72a59fad494de8eb42fe45b58f0143
natneam <se.natneam.mesele@gmail.com>
2021-02-16 17:48:29 +0300

    fetching img url using fetch api

--- gallery/assets/js/app.js
+++ gallery/assets/js/app.js
@@ -1,31 +1,34 @@
-let imageNumber = parseInt(innerWidth/120)-3
-let urlList = generateUrl()
+let imageNumber = 10
+let urlList;
 let navigator = document.getElementById("navigator");
 let imageBox = document.getElementById("imageBox");
-makeNavigator();
-makeImage()
 
-function generateUrl(){
-    let urlList = []
-    for (let i = 0; i < imageNumber; i++) {
-        urlList.push(`https://picsum.photos/id/${Math.floor(Math.random() * (1083 - 0) ) + 0}/1400/800`);
-    }
-    return urlList
+async function generateUrl(){
+    return await fetch("https://picsum.photos/v2/list?page=3&limit=10")
 }
+
+generateUrl().then((response)=>{
+    response.json().then((list)=>{
+        urlList = list
+        console.log(urlList)
+        makeNavigator();
+        makeImage()
+    })
+})
 
 function makeNavigator(){
     let output = ''
-    for (let i = 0; i < urlList.length; i++) {
-        output += `<a class="w-32 h-16 text-gray-700 bg-white flex justify-center items-center mx-1" href="#img-${i}"><img src="${urlList[i]}" alt="" class="h-full w-full rounded"></a>`
+    for (let i = 0; i < imageNumber; i++) {
+        output += `<a class="w-32 h-16 text-gray-700 bg-white flex justify-center items-center mx-1" href="#img-${i}"><img src="https://picsum.photos/id/${urlList[i].id}/1400/800" alt="" class="h-full w-full rounded"></a>`
     }
     navigator.innerHTML = output;
 }
 
 function makeImage(){
     let output = ''
-    for (let i = 0; i < urlList.length; i++) {
+    for (let i = 0; i < imageNumber; i++) {
         output += `<div class="snap-start w-full h-full flex items-center justify-center flex-shrink-0 bg-gray-100 px-10" id="img-${i}">
-        <img src="${urlList[i]}" alt="" class="rounded-xl h-full w-full">
+        <img src="https://picsum.photos/id/${urlList[i].id}/1400/800" alt="" class="rounded-xl h-full w-full">
       </div>`
     }
     imageBox.innerHTML = output
